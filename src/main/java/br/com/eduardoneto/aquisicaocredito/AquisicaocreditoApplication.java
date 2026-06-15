21b810265ea392239fdef615795a0b6bf9630125
eduardo.neto <edu.g.neto06@gmail.com>
2026-06-15 18:23:25 -0300

    Initial project setup spring boot

---
+++ src/main/java/br/com/eduardoneto/aquisicaocredito/AquisicaocreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardoneto.aquisicaocredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquisicaocreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquisicaocreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eduardoneto/aquisicaocredito/AquisicaocreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardoneto.aquisicaocredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquisicaocreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
